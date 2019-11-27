30c198436d67f375b6328475c590e31dbed1a2d4
Steven Hertz <hertzsteven@icloud.com>
2019-11-27 17:04:59 -0500

    Initial Commit

---
+++ doing settings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  doing settings
+//
+//  Created by Steven Hertz on 11/27/19.
+//  Copyright © 2019 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
