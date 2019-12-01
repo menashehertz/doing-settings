2faf4098ba8e8f54a12f060f11c53fc7eb638ce7
Steven Hertz <hertzsteven@icloud.com>
2019-12-01 11:13:40 -0500

    working even with taking to the settings screen

--- doing settings/ViewController.swift
+++ doing settings/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewDidAppear(_ animated: Bool) {
+        ShowSettings()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let msg = UserDefaults.standard.object(forKey: "my_message") as! String
+        print(msg)
+        
+//        UserDefaults.standard.set(6, forKey: "teacherSelected")
+        
+        
     }
 
+    func ShowSettings() {
+        let alertController = UIAlertController (title: "Title", message: "Go to Settings?", preferredStyle: .alert)
+        
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+            
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+            
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    print("Settings opened: \(success)") // Prints true
+                })
+            }
+        }
+        
+        alertController.addAction(settingsAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
